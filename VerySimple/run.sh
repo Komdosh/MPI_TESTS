a06cf0d883023e456784ca0d58dae7955b155aff
Andrey Tabakov <komdosh@yandex.ru>
2019-07-08 10:03:14 +0300

    update run script

--- VerySimple/run.sh
+++ VerySimple/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+LOG_PREFIX="[Run MPICH]:"
+
 MPICH_DIR="/opt/mpich"
 MPICH_TRYLOCK_DIR="/per-vni-trylock"
 MPICH_HANDOFF_DIR="/per-vni-handoff"
@@ -10,19 +12,27 @@ PROGRAM_PATH="cmake-build-debug/VerySimple"
 
 NUMBER_OF_PROCESS="2"
 
+setDebug(){
+  if test -f "./dbg*"; then
+    rm dbg*
+  fi
+
+  export MPICH_DBG_FILENAME=./dbg-%d.log
+  export MPICH_DBG_CLASS=THREAD
+  export MPICH_DBG_LEVEL=VERBOSE
+}
 
 CURRENT_MPI=""
 
-while getopts ":gp:n:" opt; do
+while getopts ":gp:n:d" opt; do
 	case $opt in
 		g) #global critical section
-      CURRENT_MPI="$MPICH_GLOBAL_DIR"
+            CURRENT_MPI="$MPICH_GLOBAL_DIR"
 			;;
 		p) #per-vni trylock
 			perVniType=${OPTARG}
-			echo $perVniType
-      if test "$perVniType" != "trylock" && test "$perVniType" != "handoff"; then
-			  echo $LOG_PREFIX "Wrong per-vni type, trylock or handoff only!"
+            if test "$perVniType" != "trylock" && test "$perVniType" != "handoff"; then
+			  echo ${LOG_PREFIX} "Wrong per-vni type, trylock or handoff only!"
 			  exit 1
 			fi
 			CURRENT_MPI="/per-vni-$perVniType"
@@ -30,10 +40,13 @@ while getopts ":gp:n:" opt; do
 		n) #per-vni handoff
 			NUMBER_OF_PROCESS="${OPTARG}"
 			;;
+		d)
+		  setDebug
+		  ;;
 		:) #empty or unknown arg
 			#do nothing
 			;;
     esac
 done
 
-eval "${MPICH_DIR}${CURRENT_MPI}${MPICH_MPIEXEC} -n ${NUMBER_OF_PROCESS} ${PROGRAM_PATH}"
+eval "${MPICH_DIR}${CURRENT_MPI}${MPICH_MPIEXEC} -host localhost -n ${NUMBER_OF_PROCESS} ${PROGRAM_PATH}"
